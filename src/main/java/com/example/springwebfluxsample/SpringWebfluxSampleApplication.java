091150a64d7865eb9266d79d9ca5eeb9f1f52060
negi524 <egtnrumpbiizarre.info@gmail.com>
2021-01-29 21:49:30 +0900

    first commit

---
+++ src/main/java/com/example/springwebfluxsample/SpringWebfluxSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebfluxsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxSampleApplication.class, args);
+	}
+
+}
