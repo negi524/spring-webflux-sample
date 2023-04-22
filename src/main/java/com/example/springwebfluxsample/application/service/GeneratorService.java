ad2f38d1a0898da3b4e6fb7bd39b79c34f968c99
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-04-22 23:21:31 +0900

    Fluxのデータを生成するサンプルコード追加

---
+++ src/main/java/com/example/springwebfluxsample/application/service/GeneratorService.java
@@ -0,0 +1,23 @@
+package com.example.springwebfluxsample.application.service;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+/**
+ * 任意のデータを生成する
+ */
+@Service
+@RequiredArgsConstructor
+public class GeneratorService {
+
+  /**
+   * 生成した名前を取得する
+   *
+   * @return 名前のリスト
+   */
+  public Flux<String> getNameFlux() {
+    return Flux.fromIterable(List.of("alex", "ben", "chloe"));
+  }
+}
