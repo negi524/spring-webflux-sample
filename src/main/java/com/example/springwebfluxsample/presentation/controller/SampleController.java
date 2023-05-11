a6db2a0c67a13a065088334fd21810609e813a67
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-11 23:27:51 +0900

    任意のデータを生成するサンプルコード追加 (#6)
    * Fluxのデータを生成するサンプルコード追加
    
    * Monoのデータを生成するサンプルコード作成

--- src/main/java/com/example/springwebfluxsample/presentation/controller/SampleController.java
+++ src/main/java/com/example/springwebfluxsample/presentation/controller/SampleController.java
@@ -1,29 +1,46 @@
 package com.example.springwebfluxsample.presentation.controller;
 
 import com.example.springwebfluxsample.application.service.ExternalApiRequestService;
+import com.example.springwebfluxsample.application.service.GeneratorService;
 import com.example.springwebfluxsample.application.service.TodoService;
 import com.example.springwebfluxsample.infrastructure.dto.response.HttpbinGetResponse;
 import com.example.springwebfluxsample.presentation.dto.response.Todo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+/**
+ * サンプルコントローラー
+ */
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
-public class TodoController {
-  private final TodoService service;
+public class SampleController {
+  private final TodoService todoService;
   private final ExternalApiRequestService externalApiRequestService;
+  private final GeneratorService generatorService;
 
   @GetMapping("/todo")
   public Mono<Todo> getTodo() {
-    return service.fetchTodo();
+    return todoService.fetchTodo();
+  }
+
+  @GetMapping("/httpbin")
+  public Mono<HttpbinGetResponse> getHttpbin() {
+    return externalApiRequestService.fetch();
   }
 
   @GetMapping("/sample")
-  public Mono<HttpbinGetResponse> getSample() {
-    return externalApiRequestService.fetch();
+  public Mono<String> getSample() {
+    generatorService.getNameFlux()
+        .subscribe(name -> log.info("Flux Name is: {}", name));
+
+    generatorService.getNameMono()
+        .subscribe(name -> log.info("Mono Name is: {}", name));
+    return Mono.just("OK");
   }
 }
