efd1ddcc0b18aecc2b0954ed10a04f8fa14a718b
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-05-24 00:22:49 +0900

    APIのサマリー情報記載

--- src/main/java/com/example/springwebfluxsample/SpringWebfluxSampleApplication.java
+++ src/main/java/com/example/springwebfluxsample/SpringWebfluxSampleApplication.java
@@ -1,9 +1,17 @@
 package com.example.springwebfluxsample;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+    title = "サンプルAPI",
+    description = "サンプルAPIの説明文",
+    version = "v1"
+)
+)
 public class SpringWebfluxSampleApplication {
 
   public static void main(final String[] args) {
--- src/main/java/com/example/springwebfluxsample/presentation/controller/SampleController.java
+++ src/main/java/com/example/springwebfluxsample/presentation/controller/SampleController.java
@@ -5,6 +5,7 @@ import com.example.springwebfluxsample.application.service.GeneratorService;
 import com.example.springwebfluxsample.application.service.TodoService;
 import com.example.springwebfluxsample.infrastructure.dto.response.HttpbinGetResponse;
 import com.example.springwebfluxsample.presentation.dto.response.Todo;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,6 +31,7 @@ public class SampleController {
    * @return todoオブジェクト
    */
   @GetMapping("/todo")
+  @ApiResponse(responseCode = "200", description = "jsonplaceholderから返却されるtodoオブジェクトを返却する")
 //  @ApiOperation(value = "jsonplaceholderのtodo情報を取得する", notes = "詳細な説明はここに記述")
 //  @ApiResponses(value = {
 //      @ApiResponse(code = 400, message = "bad request", response = ErrorResponse.class)
